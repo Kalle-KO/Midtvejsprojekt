50387df663e9fcedf03d13f3012cc279096c5767
Kalle-KO <kaot0001@stud.kea.dk>
2025-11-07 08:53:59 +0100

    Initial commit

---
+++ src/test/java/org/example/midtvejsprojekt/MidtvejsprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.midtvejsprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtvejsprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
